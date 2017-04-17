ee3b21084b87e65710aee7dff3c8053c8c71f985
Elizabeth <elizabethnich19@gmail.com>
2017-04-17 11:22:41 -0400

    Done.

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,18 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def new
+    @episode = Episode.new
+  end
+
+  def create
+    @episode = Episode.create(params[:id])
+    redirect_to episodes_path(@episode)
+  end
+
+  def show
+    @episode = Episode.find(params[:id])
+    
+  end
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,24 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.create(params[:name])
+    redirect_to guests_path(@guest)
+  end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+
+# private
+# def guest_params
+#   params.(:guest).require(:name, :occupation)
+# end
+
 end
